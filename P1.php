52cd7bba9bfef66bd044e2d5cc5daa66adfe9f2b
Rick Harraghy <rharraghy@g.harvard.edu>
2014-09-18 05:48:46 -0400

    Added P1.php; added jpg file Rick_HuntingtonRavine; added readme file.

---
+++ P1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+  <title>Rick Harraghy - Dynamic Web Apps Portfolio</title>
+</head>
+<body>
+  <h1>Rick Harraghy</h1>
+  <h1>Dynamic Web Apps Portfolio</h1>
+  <img src="Rick_HuntingtonRavine.jpg" alt="Mountain View" style="width:304px;height:228px">
+  <h1>Project 2 Title</h1>
+  <h1>Project 3 Title</h1>
+  <h1>Project 4 Title</h1>
+  <p><b>In 2+ sentences describe who you are and<br>what you do.</b><br><br>My name is Rick Harraghy, I am a software engineer at General Dynamics in Taunton, MA.</p>
+  <!-- Note that it is "h" + "1", not "h" + the letters "one" -->
+  <p>Look Ma, I am coding <abbr title="Hyper Text Markup Language">HTML</abbr>.</p>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,8 @@
+# P1: System Setup/Portfolio 09-17-14
+
+
+1. Set up a portfolio to present all the projects you'll complete in this course.
+2. Ensure your Git and server workflow is in working order.
+3. Refresh your HTML/CSS skills if needed.
+
+
