39c953cfc94589ebc832007614a07df3c7a44744
Rick Harraghy <rharraghy@g.harvard.edu>
2014-09-19 13:16:00 -0400

    Changed github link to correct Px locations.

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
   <h1>Dynamic Web Apps Portfolio</h1>
   <img src="Rick_HuntingtonRavine.JPG" alt="Mountain View" style="width:304px;height:228px">
   <h1>Project 2 Title</h1>
-  <p>View:<a href="http://p2.rickcscie15.me/">Project2 on Digital Ocean </a>GitHub:<a href="https://github.com/rickgdc4s/P1">Project2 on GitHub</a></p>
+  <p>View:<a href="http://p2.rickcscie15.me/">Project2 on Digital Ocean </a>GitHub:<a href="https://github.com/rickgdc4s/P2">Project2 on GitHub</a></p>
   <h1>Project 3 Title</h1>
-  <p>View:<a href="http://p3.rickcscie15.me/">Project3 on Digital Ocean </a>GitHub:<a href="https://github.com/rickgdc4s/P1">Project3 on GitHub</a></p>
+  <p>View:<a href="http://p3.rickcscie15.me/">Project3 on Digital Ocean </a>GitHub:<a href="https://github.com/rickgdc4s/P3">Project3 on GitHub</a></p>
   <h1>Project 4 Title</h1>
-  <p>View:<a href="http://p4.rickcscie15.me/">Project4 on Digital Ocean </a>GitHub:<a href="https://github.com/rickgdc4s/P1">Project4 on GitHub</a></p>
+  <p>View:<a href="http://p4.rickcscie15.me/">Project4 on Digital Ocean </a>GitHub:<a href="https://github.com/rickgdc4s/P4">Project4 on GitHub</a></p>
   <p><b>In 2+ sentences describe who you are and<br>what you do.</b><br><br>My name is Rick Harraghy, I am a software engineer at General Dynamics in Taunton, MA.</p>
 </body>
 </html>
